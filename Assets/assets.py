5fd47c4f7fb34d4a9ac3ddc67a1e2252d07b1f66
illusion615 <illusion615@gmail.com>
2020-05-03 15:10:50 +0800

    Code initialization

---
+++ Assets/assets.py
@@ -0,0 +1,24 @@
+teammembers = [
+    {"name": "Aubrey", "attack": 20, "live": 100, "level": 1, "experience": 0},
+    {"name": "Pipi", "attack": 10, "live": 75, "level": 1, "experience": 0}
+]
+
+enemies = [
+    {"name": "Dog", "attack": 5, "live": 10,
+        "experience_gain": 10},
+    {"name": "Tiger", "attack": 20, "live": 50,
+        "experience_gain": 20},
+    {"name": "Pirate with pistol", "attack": 10, "live": 30,
+        "experience_gain": 30},
+    {"name": "Pirate with machine gun", "attack": 50,
+        "live": 150, "experience_gain": 50},
+    {"name": "Pirate with fatboy nuke", "attack": 100,
+        "live": 150, "experience_gain": 100}
+]
+
+weapons = [
+    {"name": "Knife", "attack": 15},
+    {"name": "Pistol", "attack": 10},
+    {"name": "Machine gun", "attack": 50},
+    {"name": "Fatboy nuke", "attack": 100}
+]
---
+++ Assets/subtitles.py
@@ -0,0 +1,9 @@
+openning = [
+    'Presented by Wells & Aubrey, 2020',
+    'Pipi and Aubrey are in 2077, they have one boy called Sean.',
+    'One day nuclear war was begin, and they escaped to monkey vault 56.'
+    'They lived happily with many good friends, like crocodile and hippo.',
+    "One day , the vault overseer send Pipi and Aubrey go to wasteland for food.",
+    "When they back to the vault they can't find Sean, the overseer told them that pirate once came to the vault and they took Sean away....",
+    "Pipi and Aubury start the journey to find their son in the wasteland..."
+]
